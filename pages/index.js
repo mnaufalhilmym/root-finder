659ee5c01a336cb62e5ecb54d02bf22702cb2700
Muhammad Naufal Hilmy Makarim <mail@hilmy.dev>
2021-09-27 14:02:49 +0700

    fix layout

--- pages/index.js
+++ pages/index.js
@@ -18,34 +18,32 @@ export default function Home() {
       <Head>
         <title>Root Finder</title>
       </Head>
-      <div className="w-screen overflow-x-auto">
-        <div className="flex flex-col min-h-screen px-10 pt-10 pb-20 font-sans">
-          <Header setExpertMode={setExpertMode} />
-          <main className="text-center">
-            <div className="w-min mx-auto my-10 whitespace-nowrap">
-              <h2 className="font-black text-5xl sm:text-7xl">Root Finder</h2>
-              <h3 className="sm:text-xl">
-                Using Newton-Raphson or Secant Method
-              </h3>
-            </div>
-            <Form
+      <div className="flex flex-col min-h-screen px-10 pt-10 pb-20 font-sans">
+        <Header setExpertMode={setExpertMode} />
+        <main className="text-center">
+          <div className="w-min mx-auto my-10 whitespace-nowrap">
+            <h2 className="font-black text-5xl sm:text-7xl">Root Finder</h2>
+            <h3 className="sm:text-xl">
+              Using Newton-Raphson or Secant Method
+            </h3>
+          </div>
+          <Form
+            setShowResult={setShowResult}
+            setMethod={setMethod}
+            setData={setData}
+            expertMode={expertMode}
+            display={expertMode}
+          />
+          {!showResult && method && (
+            <RemountResult
+              showResult={showResult}
               setShowResult={setShowResult}
-              setMethod={setMethod}
-              setData={setData}
-              expertMode={expertMode}
-              display={expertMode}
             />
-            {!showResult && method && (
-              <RemountResult
-                showResult={showResult}
-                setShowResult={setShowResult}
-              />
-            )}
-            {showResult && <Result method={method} data={data} />}
-          </main>
-        </div>
-        <Footer />
+          )}
+          {showResult && <Result method={method} data={data} />}
+        </main>
       </div>
+      <Footer />
     </>
   );
 }
